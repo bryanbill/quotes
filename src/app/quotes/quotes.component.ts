a4579a2a8d7eb5f282265cea2045fd251c9183ef
bryanbill <emobrya@gmail.com>
2022-04-10 18:33:52 +0300

    feat:logic - upvote and down vote a quote

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { QuoteModel } from '../models/quote.model';
 
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
-  styleUrls: ['./quotes.component.css']
+  styleUrls: ['./quotes.component.css'],
 })
 export class QuotesComponent implements OnInit {
+  @Input()
+  quotes!: QuoteModel[];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  downVote(quote: QuoteModel): void {
+    // Reduce the quote's downVotes by 1
+    quote.downVotes--;
   }
-
+  upVote(quote: QuoteModel): void {
+    // Increase the quote's upVotes by 1
+    quote.upVotes++;
+  }
 }
